276c001791dd219385c21cf3a3eff396b49f05c6
y9Finsi <98806102+y9Finsi@users.noreply.github.com>
2023-10-04 13:59:52 +0300

    Добавлен docsrting
    ДОбавлен докстринг для файла

--- first.py
+++ first.py
@@ -1,8 +1,16 @@
-""""""
-from math import pi
-
-
 def degree(radius: float, time: float, acceleration: float, velocity: float = 0) -> float:
+    """
+    Calculate the position in degrees along the circumference of a circle given initial velocity, acceleration, and time.
+    
+    Parameters:
+    radius (float): Radius of the circle.
+    time (float): Time in seconds.
+    acceleration (float): Acceleration in meters per second squared.
+    velocity (float, optional): Initial velocity in meters per second. Default is 0.
+    
+    Returns:
+    float: Position in degrees along the circumference of the circle.
+    """
     all_degree = 360
     square = velocity + (acceleration * time ** 2) / 2
     length = 2 * pi * radius
