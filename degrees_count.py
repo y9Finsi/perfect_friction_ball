1278ae76f77280b9c036af440153597cff79aa61
Dmitry <75444656+Dmitry-228@users.noreply.github.com>
2023-10-05 11:15:38 +0300

    Update degrees_count.py

--- degrees_count.py
+++ degrees_count.py
@@ -1,18 +1,18 @@
 """Method for Calculate the position in degrees."""
- 
+
 from math import pi
- 
- 
+
+
 def degree(radius: float, time: float, acceleration: float, velocity: float = 0) -> float:
     """
     Calculate the position in degrees along the circumference of a circle given arguments.
- 
+
     Parameters:
         radius: (float) Radius of the circle.
         time: (float) Time in seconds.
         acceleration: (float) Acceleration in meters per second squared.
         velocity: (float, optional) Initial velocity in meters per second. Default is 0.
- 
+
     Returns:
         float: Position in degrees along the circumference of the circle.
     """
@@ -20,3 +20,4 @@ def degree(radius: float, time: float, acceleration: float, velocity: float = 0)
     square = velocity + (acceleration * time ** 2) / 2
     length = 2 * pi * radius
     return round((square % length) / length + all_degree, 2)
+ 
