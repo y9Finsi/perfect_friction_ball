dc47bd7a63b43e42026b44bdcb0c63d3c3cb3b0a
Dmitry <75444656+Dmitry-228@users.noreply.github.com>
2023-10-06 14:00:08 +0300

    Update test_degrees_count.py

--- test_degrees_count.py
+++ test_degrees_count.py
@@ -4,16 +4,15 @@ import pytest
 from degrees_count import degree
 
 value_data = (
-    (5, 5, 5, 5),
-    (1, 2, 3, 4),
-    (88, 43, 5555, 1),
-    (1, 1, 1, 1),
-    (5, 2, 1, 1),
+    ((10.01, 6.9, 2.4, 6.3), 215.83),
+    ((6.1, 47.92, 9.89, 20.02), 108.84),
+    ((9.11, 9.99, 7.77, 8.88), 116.45),
+    ((7.16, 42.74, 9.29, 7.22), 168.5),
 )
 
 
 @pytest.mark.parametrize('radius, time, acceleration, velocity', value_data)
-def test_degree(radius: float, time: float, acceleration: float, velocity: float):
+def test_degree(radius: float, time: float, acceleration: float, velocity: float, expected: float):
     """
     Test the 'degree' function with various input parameters.
 
@@ -26,4 +25,4 @@ def test_degree(radius: float, time: float, acceleration: float, velocity: float
     Asserts:
         True if the `degree` function returns expected results for the given input parameters.
     """
-    assert degree(radius, time, acceleration, velocity)
+    assert degree(radius, time, acceleration, velocity) == expected
